55044c61ec320f9ed8c8ae774e92ae0b907ba6d3
peepbii10 <pepbiel2003@gmail.com>
2024-03-20 12:29:20 +0100

    novetats

--- Balear Wave/assets/js/main.js
+++ Balear Wave/assets/js/main.js
@@ -188,29 +188,31 @@
    * Porfolio isotope and filter
    */
   window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-      });
-
-      let portfolioFilters = select('#portfolio-flters li', true);
-
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
+    let novetatsContainer = document.querySelector('.novetats-container');
+    if (novetatsContainer) {
+        let novetatsIsotope = new Isotope(novetatsContainer, {
+            itemSelector: '.novetats-item',
+            layoutMode: 'fitRows'
         });
-        this.classList.add('filter-active');
 
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
+        let novetatsFilters = document.querySelectorAll('#novetats-flters li');
+
+        novetatsFilters.forEach((filter) => {
+            filter.addEventListener('click', function(e) {
+                e.preventDefault();
+                novetatsFilters.forEach((el) => {
+                    el.classList.remove('filter-active');
+                });
+                this.classList.add('filter-active');
+
+                novetatsIsotope.arrange({
+                    filter: this.getAttribute('data-filter')
+                });
+            });
         });
-      }, true);
     }
+});
 
-  });
 
   /**
    * Initiate portfolio lightbox 
