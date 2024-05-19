93d9768c16b31a8d87157b9a8fb80b8b997f9623
peepbii10 <pepbiel2003@gmail.com>
2024-05-19 11:07:33 +0200

    cosas

--- Balear Wave/assets/js/festa.js
+++ Balear Wave/assets/js/festa.js
@@ -54,11 +54,11 @@ function cargarFiesta() {
                         `;
                     });
                 } else {
-                    comentariosHTML = '<p>No hay comentarios para esta fiesta.</p>';
+                    comentariosHTML = '<p>No hi ha comentaris per aquesta festa.</p>';
                 }
 
                 // Agregar el código HTML de los testimonios debajo de la descripción del evento
-                var testimoniosHTML = `<!-- ======= Testimonials ======= -->
+                var testimoniosHTML = `
                 <div class="testimonials container">
                     <div class="section-title">
                         <h2>Comentaris</h2>
@@ -67,7 +67,7 @@ function cargarFiesta() {
                         ${comentariosHTML}
                     </div>
                     <div class="owl-carousel testimonials-carousel"></div>
-                </div><!-- End Testimonials -->`;
+                </div>`;
 
                 // Insertar el código HTML de los testimonios debajo de la descripción del evento
                 var descripcionElement = document.getElementById('descripcion');
@@ -92,7 +92,7 @@ function cargarFiesta() {
                             <label for="image">Imatge:</label>
                             <input type="file" id="image" name="image" accept="image/*">
                         </div>
-                        <button type="submit">Enviar</button>
+                        <button class="btn-custom" type="submit">Enviar</button>
                     </form>
                 </div>`;
 
@@ -116,7 +116,7 @@ function cargarFiesta() {
 
                         reader.readAsDataURL(imagenInput.files[0]);
                     } else {
-                        var imagenPredeterminada = 'assets/img/testimonials/nadie.webp';
+                        var imagenPredeterminada = 'https://www.balearwave.com/assets/img/testimonials/nadie.webp';
                         agregarComentario(nombreFiesta, usuario, comentario, imagenPredeterminada);
                         location.reload(); // Recargar la página para mostrar el nuevo comentario
                     }
@@ -160,108 +160,108 @@ const comentariosPorFiesta = {
       {
         "usuario": "Antoni Fuster",
         "comentario": "La festa de Sant Joan a Menorca és una experiència que realment encanta tothom. Les fogueres brillants, les tradicionals carreres de cavalls i la joia contagiosa creen un ambient festiu únic que et fa sentir part d'una cosa especial. És una celebració que deixa una empremta duradora al cor dels qui l'experimenten.",
-        "imagen": "assets/img/testimonials/testimonials-1.jpg"
+        "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-1.jpg"
       },
       {
         "usuario": "Miquel Pons",
         "comentario": "Sant Joan a Menorca és una festa que captiva amb la seva atmosfera única i vibrant. Des de les impressionants fogueres fins a l'emocionant competència de cavalls, cada moment és ple d'emoció i d'energia. Tot i això, la multitud pot resultar aclaparadora per a alguns, encara que val la pena submergir-se en la màgia d'aquesta celebració.",
-        "imagen": "assets/img/testimonials/testimonials-4.jpg"
+        "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-4.jpg"
       }
     ],
     "Sant Antoni": [
       {
         "usuario": "Catalina Ferrer",
         "comentario": "Sant Antoni a Sa Pobla ofereix una autèntica experiència mallorquina amb les seves desfilades de dimonis i benediccions d'animals. Tot i que la multitud pot ser aclaparadora, l'ambient festiu i les tradicions locals fan que valgui la pena.",
-        "imagen": "assets/img/testimonials/testimonials-2.jpg"
+        "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-2.jpg"
       },
       {
         "usuario": "Margalida Roig",
         "comentario": "La festa de Sant Antoni a Sa Pobla és una celebració enèrgica i acolorida que destaca pels seus rituals tradicionals i la seva música típica. Tot i l'afluència de turistes, l'autenticitat d'aquesta festa la converteix en una experiència única.",
-        "imagen": "assets/img/testimonials/testimonials-3.jpg"
+        "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-3.jpg"
       }
     ],
     "La Beata": [
         {
             "usuario": "Llorenç Morey",
             "comentario": "La Beata de Santa Margalida és una festivitat arrelada a la devoció religiosa i la tradició local. Tot i que no té esdeveniments més contemporanis, ofereix una experiència enriquidora per als que busquen submergir-se en l'espiritualitat mallorquina.",
-            "imagen": "assets/img/testimonials/testimonials-5.jpg"
+            "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-5.jpg"
         },
         {
             "usuario": "Magdalena Fullana",
             "comentario": "La Beata de Santa Margalida és una celebració que reflecteix la profunda religiositat de la comunitat local. Amb les seves processons i esdeveniments culturals, ofereix una visió autèntica de la vida a l'illa, encara que potser no és tan atractiva per a alguns turistes.",
-            "imagen": "assets/img/testimonials/testimonials-2.jpg"
+            "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-2.jpg"
         }
       ],
       "La festa del Vermar": [
         {
             "usuario": "Pere Gelabert",
             "comentario": "La festa del Vermar és una celebració del vi i la collita que atrau els amants de l'enologia i la gastronomia. Tot i que la saturació de turistes li pot restar una mica d'encant local, ofereix una experiència sensorialment enriquidora amb degustacions i activitats relacionades amb el vi.",
-            "imagen": "assets/img/testimonials/testimonials-4.jpg"
+            "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-4.jpg"
         },
         {
             "usuario": "Joana Riera",
             "comentario": "La festa del Vermar és una oportunitat per gaudir de la rica cultura vinícola de la regió. Tot i la presència de turistes, les degustacions i activitats ofereixen una experiència única que destaca la importància de la tradició vinícola a l'illa.",
-            "imagen": "assets/img/testimonials/testimonials-3.jpg"
+            "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-3.jpg"
         }
       ],
       "Moros i cristians": [
         {
             "usuario": "Bartomeu Serra",
             "comentario": "Moros i Cristians és una representació acolorida i emocionant de la història i la cultura de Mallorca. Tot i que l'enfocament a la guerra pot resultar desafiant per a alguns, les desfilades i recreacions històriques són impressionants i ofereixen una visió fascinant del passat de l'illa.",
-            "imagen": "assets/img/testimonials/testimonials-1.jpg"
+            "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-1.jpg"
         },
         {
             "usuario": "Francisca Ordines",
             "comentario": "Moros i Cristians és una festa que transporta els visitants a temps passats amb les seves desfilades i recreacions històriques. Tot i que pot no ser del gust de tots a causa del seu enfocament a la guerra, ofereix una experiència única que destaca la diversitat cultural de la regió.",
-            "imagen": "assets/img/testimonials/testimonials-2.jpg"
+            "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-2.jpg"
         }
       ],
       "Festes de la Terra": [
         {
             "usuario": "Sebastià Salom",
             "comentario": "Les Festes de la Terra són una celebració diversa i emocionant de la cultura mallorquina contemporània. Tot i que la comercialització li pot restar una mica d'autenticitat, els esdeveniments culturals i concerts ofereixen una visió completa de la vida a l'illa.",
-            "imagen": "assets/img/testimonials/testimonials-5.jpg"
+            "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-5.jpg"
         },
         {
             "usuario": "Maria Florit",
             "comentario": "Les Festes de la Terra són una oportunitat per celebrar la rica cultura mallorquina. Tot i que la comercialització pot ser un inconvenient, els concerts i activitats ofereixen una experiència vibrant que destaca la diversitat cultural de la regió.",
-            "imagen": "assets/img/testimonials/testimonials-3.jpg"
+            "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-3.jpg"
         }
       ],
       "Festa de Sant Martí de Es Mercadal": [
         {
             "usuario": "Guillem Galmés",
             "comentario": "La Festa de Sant Martí en Es Mercadal es una celebración tradicional llena de encanto y hospitalidad. Aunque puede carecer de atracciones modernas, ofrece una visión auténtica de la vida en la isla y una oportunidad para conectarse con la comunidad local.",
-            "imagen": "assets/img/testimonials/testimonials-1.jpg"
+            "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-1.jpg"
         },
         {
             "usuario": "Aina Barceló",
             "comentario": "La Festa de Sant Martí a Es Mercadal és una festivitat que ofereix una experiència autèntica de la vida a l'illa. Tot i que pot no tenir el mateix atractiu per a tots els turistes a causa de la manca d'atraccions modernes, ofereix una visió enriquidora de la cultura local.",
-            "imagen": "assets/img/testimonials/testimonials-2.jpg"
+            "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-2.jpg"
         }
       ],
       "Sant Sebastià": [
         {
             "usuario": "Jaume Gual",
             "comentario": "Sant Sebastià a Palma és una festa que combina la tradició amb la modernitat de manera espectacular. Tot i que l'aglomeració de persones pot resultar aclaparadora per a alguns, els correfocs, concerts i activitats culturals ofereixen una experiència vibrant i emocionant",
-            "imagen": "assets/img/testimonials/testimonials-5.jpg"
+            "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-5.jpg"
         },
         {
             "usuario": "Antoni Mascaró",
             "comentario": "Sant Sebastià en Palma es una celebración que atrae con su atmósfera festiva y sus emocionantes actividades. Aunque puede resultar abrumadora debido a la gran cantidad de personas, ofrece una experiencia única que destaca la vitalidad de la cultura mallorquina.",
-            "imagen": "assets/img/testimonials/testimonials-4.jpg"
+            "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-4.jpg"
         }
       ],
       "El Cosso": [
         {
             "usuario": "Miquel Vidal",
             "comentario": "El Cosso es una fiesta que transporta a los visitantes a un mundo de fantasía y diversión. Aunque la saturación de turistas puede restarle algo de autenticidad, los desfiles y actividades ofrecen una experiencia mágica que deleita a toda la familia.",
-            "imagen": "assets/img/testimonials/testimonials-1.jpg"
+            "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-1.jpg"
         },
         {
             "usuario": "Aina Pons",
             "comentario": "El Cosso és una celebració impressionant que captiva amb les seves desfilades i decoracions magnífiques. Tot i que pot no ser tan autèntica a causa de la gran quantitat de turistes, ofereix una experiència màgica que transporta els visitants a un món de fantasia.",
-            "imagen": "assets/img/testimonials/testimonials-2.jpg"
+            "imagen": "https://www.balearwave.com/assets/img/testimonials/testimonials-2.jpg"
         }
       ]
   };
